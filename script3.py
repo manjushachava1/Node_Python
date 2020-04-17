7181618ced52b3499cdb16891b124e1e2d23636f
Manjusha Chava <manjushachava@gmail.com>
2020-04-17 00:02:22 -0400

    can parse a json file now

--- script3.py
+++ script3.py
@@ -1,5 +1,18 @@
-import json
-
-with open('countries.json') as json_data:
-	for entry in json_data:
-	 print(entry)
+# Python program to read 
+# json file  
+import json 
+  
+# Opening JSON file 
+f = open('countries.json',) 
+  
+# returns JSON object as  
+# a dictionary 
+data = json.load(f) 
+  
+# Iterating through the json 
+# list 
+for i in data: 
+    print(i['name'])
+  
+# Closing file 
+f.close() 
